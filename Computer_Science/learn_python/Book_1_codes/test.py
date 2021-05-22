019c64d871b6470d2e5c768530476568324088c9
VINOD <raita0100@gmail.com>
2021-05-22 18:54:15 +0530

    added new test file at Book-1-code

---
+++ Computer_Science/learn_python/Book_1_codes/test.py
@@ -0,0 +1,18 @@
+'''Simple Test program
+written by me
+'''
+__author__ = "Vinod.A.P"
+__email__ = "raita0100@gmail.com"
+__date__ = "22 may 2021 sat 18:46"
+__version__ = 1.0
+
+
+__copyright__ = ""
+__credits__ = []
+__license__ = ""
+__maintainer__ = "Vinod A P"
+
+__status__ = "Production"
+
+if __name__ == '__main__':
+	print("Hello Friend")
